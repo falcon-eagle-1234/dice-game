10380cdffb9bda3db15aee17fd1caca236621cfa
chinbaa <janchivchinbaa@gmail.com>
2024-04-01 10:25:49 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч
 var activePlayer   = 1;
 
+
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
+
 
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 
-// Шооны талуудын оноог random-оор үүсгэнэ.
-var dice = Math.floor(Math.random() * 6) +1;
 
+// Шооны талуудын оноог random-оор үүсгэнэ.
+
+
+// var diceNumber = Math.floor(Math.random() * 6) +1;
 // window.document.querySelector('#score-0').textContent = dice;
 // document.querySelector('#score-1').textContent = dice;
 // document.querySelector('#score-1').innerHTML = "<em>Yes ?</em>";
-
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
+// window.document.querySelector('#score-0').textContent = 0;
 
 
-window.document.querySelector('#current-0').textContent = 0;
-window.document.querySelector('#current-1').textContent = 0;
+window.document.getElementById("score-0").textContent = 0;
+window.document.getElementById('score-1').textContent = 0;
+window.document.getElementById('current-0').textContent = 0;
+window.document.getElementById('current-1').textContent = 0;
 
-document.querySelector('.dice').style.display = "none";
-console.log('Шоо :' + dice);
+var diceDom = document.querySelector(".dice");
+
+// document.querySelector('.dice').style.display = "none";
+
+diceDom.style.display = "none";
+
+document.querySelector('.btn-roll').addEventListener("click",function shooShid(){
+    var diceNumber = Math.floor(Math.random() * 6) +1;
+    diceDom.style.display = "block";
+    diceDom.src ="dice-" + diceNumber + ".png";
+
+    // alert('Shoo buulaa : ' + diceNumber)
+});
+
+// document.querySelector('.btn-roll').addEventListener("click", shooShid);
+// function shooShid(){
+//     var diceNumber = Math.floor(Math.random() * 6) +1;
+//     alert('Shoo buulaa : ' + diceNumber)
+// }
+// console.log('Шоо :' + diceNumber);
