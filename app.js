7a167a85ef5443f23b58f2fbcc72ced396053566
chinbaa <janchivchinbaa@gmail.com>
2024-04-02 09:18:23 +0800

    onoo tsugluulah

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer   = 1;
+var activePlayer   = 0;
 
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
@@ -36,8 +36,75 @@ document.querySelector('.btn-roll').addEventListener("click",function shooShid()
     diceDom.style.display = "block";
     diceDom.src ="dice-" + diceNumber + ".png";
 
+    if(diceNumber !== 1){
+
+        roundScore = roundScore + diceNumber
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }else{
+
+        nextswitchPlayer();
+
+
+        
+        // if(activePlayer === 0){
+        //     activePlayer = 1;
+        // }else{
+        //     activePlayer = 0;
+        // }
+
+    }
+
     // alert('Shoo buulaa : ' + diceNumber)
 });
+
+
+document.querySelector('.btn-hold').addEventListener("click", function(){
+
+
+
+    scores[activePlayer] = scores[activePlayer] + roundScore
+
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    if(scores[activePlayer] >= 100){
+
+        document.getElementById("name-" + activePlayer).textContent = "WINNER !!!"
+
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    }else{
+
+        nextswitchPlayer();
+
+    }
+
+    
+
+    // roundScore = 0;
+
+    // document.getElementById('current-' + activePlayer).textContent = "0";
+
+    
+
+})
+
+
+function nextswitchPlayer(){
+    roundScore = 0;
+
+    document.getElementById('current-' + activePlayer).textContent = 0;
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+
+    diceDom.style.display = "none";
+
+}
+
 
 // document.querySelector('.btn-roll').addEventListener("click", shooShid);
 // function shooShid(){
@@ -45,3 +112,6 @@ document.querySelector('.btn-roll').addEventListener("click",function shooShid()
 //     alert('Shoo buulaa : ' + diceNumber)
 // }
 // console.log('Шоо :' + diceNumber);
+
+
+document.querySelector('btn-new')
